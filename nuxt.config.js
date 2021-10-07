c53f3a6de2a15ce7573142b924b6263feda960d6
opeyemidy <opeyemidy@gmail.com>
2021-10-07 16:03:01 +0100

    add static target to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import baseURL from './endpoints/baseUrl'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
   head: {
     title:
       'Buzzle: Sell and Redeem Gift Cards For Naira - 100% \nReliable-Trusted-Effictent-convenient Platform',
