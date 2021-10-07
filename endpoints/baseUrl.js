898190102fcd7aaee11857bff62f307f5c65547a
opeyemidy <opeyemidy@gmail.com>
2021-10-07 15:59:08 +0100

    change base url to remote server

--- endpoints/baseUrl.js
+++ endpoints/baseUrl.js
@@ -1,3 +1,3 @@
-// const baseUrl = 'https://cardex-api.herokuapp.com/v1'
-const baseUrl = 'http://localhost:5000/v1'
+const baseUrl = 'https://cardex-api.herokuapp.com/v1'
+// const baseUrl = 'http://localhost:5000/v1'
 export default baseUrl
